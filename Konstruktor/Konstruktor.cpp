94c061e607cd7dcb5fd20bfd702d0cc07c1487b9
Rangga857 <azharrangga28@gmail.com>
2023-05-15 14:24:29 +0700

    menambahkan parameter pada mahasiswa

--- Konstruktor/Konstruktor.cpp
+++ Konstruktor/Konstruktor.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 class mahasiswa {
+private :
+	int nim;
+	string nama;
+public : 
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	
 
 };
