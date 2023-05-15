fa37901388268cf138f8ab5e68c8e209c1fdbf66
Rangga857 <azharrangga28@gmail.com>
2023-05-15 13:28:04 +0700

    membuat definisi member function

--- Destruktor/Destruktor.cpp
+++ Destruktor/Destruktor.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 class angka {
-
+private : 
+	int* arr;
+	int panjang;
+public : 
+	angka(int); // Constructor
+	~angka(); // destructor
+	void cetakData();
+	void isiData();
 };
+//definisi member function
+angka::angka(int i) { //Constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() {//Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array sudah dilepaskan" << endl;
+}
